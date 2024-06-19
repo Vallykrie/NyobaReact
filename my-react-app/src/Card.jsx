e096c0d9138afdcd89f2b07ec4e4e490ab71a4ce
Nathan Sudiara <nathansudiara@gmail.com>
2024-06-20 01:46:15 +0700

    Update Card component to include profile picture

--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -1,7 +1,7 @@
 function Card() {
     return(
         <div className="card">
-            <img src="" alt="" />
+            <img src="https://i.pinimg.com/originals/8d/12/49/8d1249009c78480d4f773714179f8d8f.jpg" alt="profile picture" />
             <h2>thanathanan_</h2>
             <p>Hello there :D I am nathan nice to meet you</p>
         </div>
