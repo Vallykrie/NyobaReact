4c29189445a43be9807f4bfe89dfdf21bca0b2f3
Nathan Sudiara <nathansudiara@gmail.com>
2024-06-21 01:09:58 +0700

    feat: Add Counter component for incrementing and decrementing a count

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -5,6 +5,7 @@ import Student from "./function/Student.jsx";
 import ListData from "./function/ListData.jsx";
 import Button from "./function/Button.jsx";
 import ProfilePicture from "./function/ProfilePicture.jsx";
+import Counter from "./counter/Counter.jsx";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Card title="thanathanan" paragraph="Hello there :D I am nathan nice to meet you " img="https://i.pinimg.com/originals/8d/12/49/8d1249009c78480d4f773714179f8d8f.jpg"/>
       <Card title="aya" paragraph="uyauya uya uya uayaya poo" img="https://i.pinimg.com/originals/44/9f/60/449f603a98344a815256f292d6c8731a.jpg"></Card>
+      <Counter></Counter>
       <Button />
       <Student name="Nathan" age={18} gender={true}></Student>
       <Student name="Naya" age={19} gender={false}></Student>
--- my-react-app/src/function/Button.jsx
+++ my-react-app/src/function/Button.jsx
@@ -1,20 +1,21 @@
 function Button() {
+  // let count = 0;
+  // const handleClick = (name) => {
+  //     if (count < 3) {
+  //         count++;
+  //         console.log(`Hello ${name} you clicked me ${count} time/s`);
+  //     } else {
+  //         console.log(`${name} stop clicking me 😡`);
+  //     }
+  // };
 
-    // let count = 0;
-    // const handleClick = (name) => {
-    //     if (count < 3) {
-    //         count++;
-    //         console.log(`Hello ${name} you clicked me ${count} time/s`);
-    //     } else {
-    //         console.log(`${name} stop clicking me 😡`);
-    //     }
-    // };
+  const handleClick = (e) => (e.target.textContent = "Ouch! 😡");
 
-    const handleClick = (e) => e.target.textContent = "Ouch! 😡";
-
-    return(
-        <button onDoubleClick={(e) => handleClick(e)}>Click me ✨</button>
-    );
+  return (
+    <div style={{ textAlign: "center" }}>
+      <button onDoubleClick={(e) => handleClick(e)}>Click me ✨</button>
+    </div>
+  );
 }
 
-export default Button
+export default Button;
