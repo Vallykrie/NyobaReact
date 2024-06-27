33cb6be46ce2b5cb4ad98a54067d051d64baf7e2
Nathan Sudiara <nathansudiara@gmail.com>
2024-06-27 22:59:53 +0700

    Remove unused components and update dependencies

--- nyoba-personal-website/src/App.jsx
+++ nyoba-personal-website/src/App.jsx
@@ -1,26 +1,10 @@
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Header from "./components/Header";
-import Home from "./components/Home";
-import AboutMe from "./components/AboutMe";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about-me" element={<AboutMe />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <Header></Header>
+    </div>
   );
 }
 
--- nyoba-personal-website/src/components/Header.jsx
+++ nyoba-personal-website/src/components/Header.jsx
@@ -1,37 +1,127 @@
-import { Link } from "react-router-dom";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
 
-const Header = () => {
+const pages = ["about me", "skills", "portfolio", "contact me"];
+
+function Header() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
-    <div className="header">
-      <div className="header-left">
-        <div className="logo">
-          <div className="logo-icon">
-            <div className="logo-shape">
-              <div className="logo-part"></div>
-              <div className="logo-part-diagonal"></div>
-            </div>
-          </div>
-        </div>
-        <div className="title">
-          <Link to="/">Personal</Link>
-        </div>
-      </div>
-      <div className="header-right">
-        <Link to="/about-me" className="header-link">
-          About Me
-        </Link>
-        <Link to="/skills" className="header-link">
-          Skills
-        </Link>
-        <Link to="/projects" className="header-link">
-          Projects
-        </Link>
-        <Link to="/contact" className="header-link">
-          Contact Me
-        </Link>
-      </div>
-    </div>
-  );
-};
+    <AppBar style={{ backgroundColor: "black", paddingLeft: "5vw", paddingRight: "5vw" }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#home"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "Bebas Neue",
+              fontWeight: "400",
+              fontStyle: "normal",
+              letterSpacing: ".7rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            NATHAN
+          </Typography>
 
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+          </Box>
+
+          <Box sx={{ flexGrow: 1, display: "flex", justifyContent: "center" }}>
+            <Box sx={{ display: { xs: "flex", md: "none" } }}>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Box>
+
+          <Typography
+            variant="h4"
+            noWrap
+            component="a"
+            href="#home"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "Bebas Neue",
+              fontWeight: "500",
+              fontStyle: "normal",
+              letterSpacing: ".7rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            Than
+          </Typography>
+
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
 export default Header;
